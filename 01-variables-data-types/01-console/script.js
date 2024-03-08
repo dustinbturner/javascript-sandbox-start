5c516f4e45bbf7f407bee17c6da7d6e3241457e2
Dustin Turner <dustin@dustinbturner.com>
2024-03-08 13:58:02 -0500

    traversy js

---
+++ 01-variables-data-types/01-console/script.js
@@ -0,0 +1,24 @@
+// This is a single line comment
+
+// 
+
+// console.log(100);
+
+console.log('hello, world');
+
+console.log(20, 'hello', true);
+
+console.log(20, 'hello', true, [1, 2, 3]);
+
+const x = 100;
+
+console.log(x);
+
+const styles = 'padding: 10px; background-color: red; color: white;';
+
+console.log('%chello, world', styles);
+
+
+
+
+
